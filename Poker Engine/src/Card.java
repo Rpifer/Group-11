7564a8aaae9899bc432cc0698c2a3a4c353fc7e2
Carey Blackwell <slowski@udel.edu>
2015-03-01 20:39:39 -0500

    commit

--- Poker Engine/src/Card.java
+++ Poker Engine/src/Card.java
@@ -1,4 +1,20 @@
 
 public class Card {
-
+	private eSuit suit;
+	private eValue value;
+	
+	private Card(){
+		
+	}
+	
+	private Card(eSuit suit, eValue value){
+		this.suit = suit;
+		this.value = value;
+	}
+	public eSuit getSuit(){
+		return this.suit;
+	}
+	public eValue getValue(){
+		return this.value;
+	}
 }
---
+++ Poker Engine/src/eSuit.java
@@ -0,0 +1,8 @@
+
+public enum eSuit {
+	SPADES,
+	HEARTS,
+	CLUBS,
+	DIAMONDS;
+
+}
